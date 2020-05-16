200a47b9e2e6fe5fd12fc717ae2b6faaa5e324d9
Bartosz Walczak <bl4kee@wp.pl>
2020-05-16 14:18:24 +0200

    added customers list, added DAO, added css

---
+++ src/com/tracker/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.tracker.springdemo.dao;
+
+import java.util.List;
+
+import com.tracker.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
