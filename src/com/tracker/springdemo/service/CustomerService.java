61565329f872a88e3ec6fa8bec14cbdebf1605df
Bartosz Walczak <bl4kee@wp.pl>
2020-05-17 16:39:47 +0200

    Added service layer, updated mappings

---
+++ src/com/tracker/springdemo/service/CustomerService.java
@@ -0,0 +1,9 @@
+package com.tracker.springdemo.service;
+
+import java.util.List;
+import com.tracker.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+}
